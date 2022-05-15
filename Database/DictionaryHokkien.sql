45886a8aa0cba79b6cee27809efb659699aeb89e
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2022-05-14 21:11:38 -0400

    Deleting Everything

--- Database/DictionaryHokkien.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[user] (
-    [userID]   NVARCHAR (450) NOT NULL,
-    [email]    NVARCHAR (MAX) NOT NULL,
-    [psswrd]   NVARCHAR (MAX) NOT NULL,
-    [username] NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED ([userID] ASC)
-);
-
--- Database/dbo.RegionalSuggest.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[RegionalSuggest] (
-    [RegionalKey]   INT REFERENCES [dbo].[regionalPronunciation] ([key]),
-    [Name]          NVARCHAR (MAX),
-    [Pronunciation] NVARCHAR (MAX),
-    [Hanji]         NVARCHAR (MAX),
-    [Key]           INT IDENTITY (1, 1) PRIMARY KEY,
-    [VocabsSuggestKey] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[VocabsSuggest] ([Key])
-);
