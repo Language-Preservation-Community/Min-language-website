e6c9b33a974456b01997bb0704c34f819509c1b0
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2022-05-15 00:22:28 -0400

    More comments, next will be setting up Controllers and Views

--- LearnLanguagesWebsite/Models/Dictionary.cs
+++ LearnLanguagesWebsite/Models/Dictionary.cs
@@ -5,6 +5,10 @@ using System.Web;
 
 namespace LearnLanguagesWebsite.Models
 {
+    // Programmer will only refer to here
+
+    // So database will be looking like 
+    // Teochew, Vocabs (likes, you, me) with regional pronunciations
     public class Dictionary
     {
         //public string LanguageName;
