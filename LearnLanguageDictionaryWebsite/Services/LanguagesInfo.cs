57b42437b68a3ce5042dc5521767900c0fcc9e99
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2022-05-24 23:51:58 -0400

    Making Language bar work

--- LearnLanguageDictionaryWebsite/Services/LanguagesInfo.cs
+++ LearnLanguageDictionaryWebsite/Services/LanguagesInfo.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LearnLanguagesWebsite.Services
+namespace LearnLanguagesDictionaryWebsite.Services
 {
     public class LanguagesInfo
     {
